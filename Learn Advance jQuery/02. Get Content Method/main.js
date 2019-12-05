bae45c928f3d5d1dd683ba05ef4cdccd17eadd26
LucianoSassano <54543892+LucianoSassano@users.noreply.github.com>
2019-12-05 17:15:32 -0300

    Add files via upload

---
+++ Learn Advance jQuery/02. Get Content Method/main.js
@@ -0,0 +1,16 @@
+/*$(document).ready(function(){
+	$("#btn1").click(function(){
+		alert("Text :" + $("#test").text());
+	});
+	$("#btn2").click(function(){
+		alert("HTML:" + $("#test").html());
+
+	});
+});*/
+
+$(document).ready(function(){
+	$("#btn3").click(function(){
+		alert("Text :" + $("#test").val());
+
+	});
+});
